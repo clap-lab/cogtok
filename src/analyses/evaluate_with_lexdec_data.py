b7423602c18893cd34600d1cdc0cf9b07f6dda5c
Yuval Pinter <yuvalpinter@users.noreply.github.com>
2023-10-23 16:10:52 +0300

    that commented-out line was actually pretty important

--- src/analyses/evaluate_with_lexdec_data.py
+++ src/analyses/evaluate_with_lexdec_data.py
@@ -7,13 +7,14 @@ evalpath = "../../data/eval/"
 resultpath = "../../results/"
 outdir = resultpath + "results_overview/"
 
+
 # Collect all outputs of all trained trained_models in a single file
 def collect_all_outputs(langs):
     for lang in langs:
         print(lang)
 
         evaldata = pd.read_csv(open(evalpath + lang + ".txt", "r"), delimiter="\t")
-        modelpath = resultpath   +"trained_models/"+ lang +"/models/"
+        modelpath = resultpath + "trained_models/" + lang + "/models/"
         models = []
         outputs = {key: [] for key in evaldata["spelling"]}
 
@@ -37,7 +38,7 @@ def collect_all_outputs(langs):
                     previous_outputs.append([])
                 outputs[key] = previous_outputs
 
-    # This is a bit of a duplication but collecting the data in pandas directly costs too much memory
+        # This is a bit of a duplication but collecting the data in pandas directly costs too much memory
         columns = ["spelling"] + ["Model_" + x for x in models]
 
         outputdata = pd.DataFrame(columns=columns)
@@ -46,25 +47,26 @@ def collect_all_outputs(langs):
             outputdata["Model_" + model] = [item[i] for item in outputs.values()]
 
         # Save a copy in case merge goes wrong
-        outputdata.to_csv(outdir  + lang + "_onlyoutput.csv", index = False)
+        outputdata.to_csv(outdir + lang + "_onlyoutput.csv", index=False)
         evaldata = evaldata.merge(outputdata, how="left", on="spelling")
-        evaldata.to_csv(outdir  + lang + ".csv", index = False)
+        evaldata.to_csv(outdir + lang + ".csv", index=False)
 
-langs = ["en", "nl", "fr", "es"]
-#collect_all_outputs(langs)
+
+langs = {"eng": "en", "nld": "nl", "fra": "fr", "spa": "es"}
+# collect_all_outputs(langs)
 
 # Evaluate with lexical decision data
 print("Not significant correlations at p<0.01")
-for lang in langs:
+for lang_l, lang_s in langs.items():
 
-    evaldata = pd.read_csv(resultpath + "results_overview/" + lang + ".csv")
+    evaldata = pd.read_csv(resultpath + "results_overview/" + lang_s + ".csv")
     evaldata = evaldata.dropna()
-    words = evaldata[evaldata["lexicality"]=="W"]
-    nonwords = evaldata[evaldata["lexicality"]== "N"]
+    words = evaldata[evaldata["lexicality"] == "W"]
+    nonwords = evaldata[evaldata["lexicality"] == "N"]
 
     datasets = [words, nonwords]
     all_results = {}
-    print(lang)
+    print(lang_l)
     for category in ["words", "nonwords"]:
         print(category)
         if category == "words":
@@ -85,12 +87,11 @@ for lang in langs:
         for model in dataset:
             print(model)
             if model.startswith("Model"):
-
                 # splits in model output
                 splits = list(dataset[model].apply(literal_eval))
-                num_splits = [len(x)-1 for x in splits]
+                num_splits = [len(x) - 1 for x in splits]
                 max = len(dataset[model])
-                wordiness = [1-(len(splits[i])/len(str(tokens[i]))) for i in range(max)]
+                wordiness = [1 - (len(splits[i]) / len(str(tokens[i]))) for i in range(max)]
 
                 # correlation
                 corr1, p1 = pearsonr(num_splits, rts)
@@ -98,16 +99,17 @@ for lang in langs:
                 corr3, p3 = pearsonr(wordiness, rts)
                 corr4, p4 = pearsonr(wordiness, accs)
 
-                results = [model, "{:.2f}".format(corr1) , "{:.2f}".format(corr2),"{:.2f}".format(corr3), "{:.2f}".format(corr4)]
+                results = [model, "{:.2f}".format(corr1), "{:.2f}".format(corr2), "{:.2f}".format(corr3),
+                           "{:.2f}".format(corr4)]
                 category_results.append(results)
-                #print(results)
-        #         if p3 >= 0.05:
-        #             print("not significant", category, lang, model, "reading time", p3)
-        #         if p4 >= 0.05:
-        #             print("not significant",category, lang, model, "accuracy", p4)
-        # all_results[category] = category_results
+                # print(results)
+                #         if p3 >= 0.05:
+                #             print("not significant", category, lang, model, "reading time", p3)
+                #         if p4 >= 0.05:
+                #             print("not significant",category, lang, model, "accuracy", p4)
+                all_results[category] = category_results
 
-    with open(outdir +  lang + "_correlations.csv", "w") as outfile:
+    with open(outdir + lang_l + "_correlations.csv", "w", encoding="utf-8") as outfile:
         for category in all_results.keys():
             outfile.write(category)
             outfile.write("\n")
@@ -122,4 +124,3 @@ for lang in langs:
 
                 outfile.write(",".join([name, size, result1, result2, result3, result4]))
                 outfile.write("\n")
-
